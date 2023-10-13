9eba8a0d5273b2f7da6683d007205403f4a007ae
Mohammad Mubassir <147790802+mubassir1243@users.noreply.github.com>
2023-10-12 22:28:11 -0700

    new

---
+++ heart.java
@@ -0,0 +1,29 @@
+class finger{
+	void finger(){
+		System.out.println("finger..");
+	}
+}
+class heart{
+	public heart(){
+		System.out.println("heart..");
+	}
+	}
+	class hand{
+		void hand(){
+			System.out.println("hand..");
+		}
+	}
+	class brain{
+		public brain(){
+			System.out.println("brain..");
+		}
+		}
+		class human{
+			void human(){
+				System.out.println("huaman..");
+			}
+			public static void main (String args){
+				finger a=new finger();
+				a.finger();
+			}
+		}
